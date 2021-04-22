82b3bce6a4057a460b08ba53a1a69a355f15de34
xDaijobu <christwurangian@gmail.com>
2021-04-22 08:33:07 +0700

    initial commit

---
+++ faceRecognationV2/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using faceRecognationV2.ViewModels;
+using faceRecognationV2.Views;
+using Xamarin.Forms;
+
+namespace faceRecognationV2
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ faceRecognationV2/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using faceRecognationV2.ViewModels;
+
+namespace faceRecognationV2.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
